fixed masking issue in head

pytorch_frost/modules/head.py
@@ -41,11 +41,13 @@ class PooledHeadLayer(nn.Module):
         else:
             raise NotImplementedError(f"pooling choice {pooling} is not implemented.")   
         
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: Tensor, mask: Optional[Tensor]=None) -> Tensor:
         
         # x: (B, N, d)
         
-        x = self.pool(x) # (B, d) -- (B, N, d) if no pooling
+        mask = mask if mask is not None else torch.zeros_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
+        
+        x = self.pool(x, mask) # (B, d) -- (B, N, d) if no pooling
         x = self.head(x) # (B, T) -- (B, N, T) if no pooling
         x = self.activation(x)
         res = {}

pytorch_frost/modules/utils.py
@@ -17,15 +17,15 @@ class GlobalAttentionPool1d(nn.Module):
         x: Tensor of shape (..., F, d)
         """
     
-        mask = mask if mask is not None else torch.ones_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
+        mask = mask if mask is not None else torch.zeros_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
     
         q = self.query
         
         attention_scores = torch.matmul(x, q.transpose(-2, -1)).transpose(-2, -1) # (..., 1, F)
-        attention_scores = torch.where( ~mask, torch.tensor(float('-inf'), device=attention_scores.device), attention_scores)
+        attention_scores = torch.where(mask, torch.tensor(float('-inf'), device=attention_scores.device), attention_scores)
     
         attention_weights = nn.functional.softmax(attention_scores, dim=-1) # (..., 1, F)
-        attention_weights = torch.where(~mask, torch.tensor(float(0.0), device=attention_weights.device), attention_weights)
+        attention_weights = torch.where(mask, torch.tensor(float(0.0), device=attention_weights.device), attention_weights)
         
         attention_output = torch.matmul(attention_weights, x) # (..., 1, d)
         res = attention_output.squeeze(-2)
@@ -45,17 +45,17 @@ class GlobalProjectedAttentionPool1d(GlobalAttentionPool1d):
         x: Tensor of shape (..., F, d)
         """
         
-        mask = mask if mask is not None else torch.ones_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
+        mask = mask if mask is not None else torch.zeros_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
 
         q = self.query
         K = self.k_proj(x)
         V = self.v_proj(x)
 
         attention_scores = torch.matmul(x, q.transpose(-2, -1)).transpose(-2, -1) # (..., 1, F)
-        attention_scores = torch.where( ~mask, torch.tensor(float('-inf'), device=attention_scores.device), attention_scores)
+        attention_scores = torch.where(mask, torch.tensor(float('-inf'), device=attention_scores.device), attention_scores)
     
         attention_weights = nn.functional.softmax(attention_scores, dim=-1) # (..., 1, F)
-        attention_weights = torch.where(~mask, torch.tensor(float(0.0), device=attention_weights.device), attention_weights)
+        attention_weights = torch.where(mask, torch.tensor(float(0.0), device=attention_weights.device), attention_weights)
         
         attention_output = torch.matmul(attention_weights, torch.nan_to_num(V, nan=0.0)) # (..., 1, d)
         res = attention_output.squeeze(-2)
@@ -69,10 +69,10 @@ class MeanPool1d(nn.Module):
         
     def forward(self, x: Tensor, mask: Optional[Tensor]=None) -> Tensor:
         
-        mask = mask if mask is not None else torch.ones_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
+        mask = mask if mask is not None else torch.zeros_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
         # (B, N, 1, F) and x is (B, N, F, d)
         
-        x = torch.where(~mask.transpose(-1, -2), torch.tensor(float(0.0), device=x.device), x)
+        x = torch.where(mask.transpose(-1, -2), torch.tensor(float(0.0), device=x.device), x)
         
         return self.pool(x.transpose(-1, -2)).transpose(-1, -2).squeeze(1)
     
@@ -82,17 +82,17 @@ class SumPool1d(nn.Module):
         
     def forward(self, x: Tensor, mask: Optional[Tensor]=None) -> Tensor:
         
-        mask = mask if mask is not None else torch.ones_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
+        mask = mask if mask is not None else torch.zeros_like(x, dtype=torch.bool).all(dim=-1, keepdim=True).transpose(-1, -2)
         # (B, N, 1, F) and x is (B, N, F, d)
         
-        x = torch.where(~mask.transpose(-1, -2), torch.tensor(float(0.0), device=x.device), x)
+        x = torch.where(mask.transpose(-1, -2), torch.tensor(float(0.0), device=x.device), x)
         return torch.sum(x, dim=-2)
     
 class NoPool1d(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: Tensor, mask: Optional[Tensor]=None) -> Tensor:
         
         return x
     